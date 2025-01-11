357e1231f5625e3689ddc70308e3ef4e4660c012
Abdulla <55580529+pikasu2000@users.noreply.github.com>
2025-01-11 20:08:10 +0530

    Update Problem15.js

--- Week3/Tuesday/Problem15.js
+++ Week3/Tuesday/Problem15.js
@@ -16,7 +16,15 @@ function CircularTraversal(Rows,Columns){
                 bag+=arr[i][0]
 
     }
-
+    for(let j =1;j<arr.length;j++){
+      bag+=arr[0][j]
+    }
+    for(let k =1;k<arr.length;k++){
+      bag+=arr[k][2]
+    }
+      bag+=arr[2][1]
+    
+    console.log(bag)
 }
 
 
